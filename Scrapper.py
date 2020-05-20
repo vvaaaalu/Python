6737d8ed6ff88e307ee31b978c440fb23f72301c
vvaaaalu <xpfjehddn.kdw@gmail.com>
2020-05-20 12:01:46 +0900

    version0520_2

--- Scrapper.py
+++ Scrapper.py
@@ -1,7 +1,9 @@
 #Web Scrapper
 import requests
 from bs4 import BeautifulSoup
-
+URL="https://www.indeed.com/jobs?q=python&limit=50"
+#definition
+def extract_indeed_pages():
 indeed_result = requests.get(
 "https://www.indeed.com/jobs?q=python&limit=50"
 )
@@ -19,3 +21,16 @@ max=pages[-1]
 
 for n in range(max):
     print(f"start={n*50}")
+
+def extract_indeed_jobs(last_page):
+    jobs=[]
+    #for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    resluts=soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+    for result in results:
+        title = result.find("div", {"class":"title"}).find("a")["title"]
+    return jobs
+
+last_indeed_page=extract_indeed_pages()
+indeed_jobs=extract_indeed_jobs(last_indeed_page)
