00658dfa2e0f410b747d4224585ac0735711d77c
vvaaaalu <xpfjehddn.kdw@gmail.com>
2020-05-20 14:24:58 +0900

    check_"name 'results' not defined"

--- Scrapper.py
+++ Scrapper.py
@@ -1,36 +1,30 @@
 #Web Scrapper
 import requests
 from bs4 import BeautifulSoup
-URL="https://www.indeed.com/jobs?q=python&limit=50"
+START = 50
+URL = "https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 #definition
 def extract_indeed_pages():
-indeed_result = requests.get(
-"https://www.indeed.com/jobs?q=python&limit=50"
-)
-
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-
-pagination = indeed_soup.find("div", {"class":"pagination"})
-
-links = pagination.find_all('a')
-pages = []
-for link in links[:-1]:
-    pages.append(int(link.string))
-
-max=pages[-1]
-
-for n in range(max):
-    print(f"start={n*50}")
+    indeed_result = requests.get(URL)
+    indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+    pagination = indeed_soup.find("div", {"class":"pagination"})
+    links = pagination.find_all('a')
+    pages = []
+    for link in links[:-1]:
+        pages.append(int(link.string))
+    max_page =pages[-1]
+    return max_page
+#for n in range(max):
+#    print(f"start={n*50}")
 
 def extract_indeed_jobs(last_page):
     jobs=[]
     #for page in range(last_page):
-    result = requests.get(f"{URL}&start={0*LIMIT}")
+    result = requests.get(f"{URL}&start={0*START}")
     soup = BeautifulSoup(result.text, "html.parser")
     resluts=soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
     for result in results:
-        title = result.find("div", {"class":"title"}).find("a")["title"]
+        job=extract_indeed_jobs(result).jobs.append(job)
     return jobs
-
 last_indeed_page=extract_indeed_pages()
 indeed_jobs=extract_indeed_jobs(last_indeed_page)
